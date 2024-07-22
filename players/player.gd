a3f4fdd78db19e536c73a5fd41789e9d69790d98
Mysti <mystievous@gmail.com>
2024-07-21 17:47:41 -0700

    rename fields

--- players/player.gd
+++ players/player.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
 @export var sprite: AnimatedSprite2D
-@export var damage: float = 5
-@export var attack_cooldown := 1.0;
+@export var interact_damage: float = 5
+@export var interact_cooldown := 0.5;
 
-var _current_attack_cooldown := 0.0;
+var _current_interact_cooldown := 0.0;
 
 const SPEED := 150.0
 
 var current_interactables: Array[Node2D] = []
 
 func _process(delta):
-	if (_current_attack_cooldown > 0):
-		_current_attack_cooldown = maxf(_current_attack_cooldown - (1 * delta), 0.0)
+	if (_current_interact_cooldown > 0):
+		_current_interact_cooldown = maxf(_current_interact_cooldown - (1 * delta), 0.0)
 	pass
 
 func _physics_process(_delta):
@@ -36,13 +36,13 @@ func _check_flip():
 
 func _input(event):
 	if event.is_action_pressed("interact"):
-		if (_current_attack_cooldown == 0.0):
+		if (_current_interact_cooldown == 0.0):
 			on_interact()
-			_current_attack_cooldown += attack_cooldown
+			_current_interact_cooldown += interact_cooldown
 
 func on_interact():
 	for interactable in current_interactables:
-		interactable.interact(damage)
+		interactable.interact(interact_damage)
 
 func _on_area_2d_area_entered(area):
 	if (area.has_method("interact") && !current_interactables.has(area)):
